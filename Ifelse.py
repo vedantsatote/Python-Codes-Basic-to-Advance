8c1e3660229610d8f12d842d1cfd290810f0a758
vedantsatote <satoteonline@gmail.com>
2024-07-21 17:30:39 +0530

    Add files via upload

---
+++ Ifelse.py
@@ -0,0 +1,10 @@
+light = input("light colour : ")
+
+if(light == "red"):
+  print("stop")
+elif(light =="yellow"):
+ print("look")
+elif(light == "green"):
+  print("go")
+else:
+  print("light is broken")
