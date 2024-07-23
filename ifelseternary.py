6c69ee1448142716239b129827b3fd6d2a62f8c5
vedantsatote <satoteonline@gmail.com>
2024-07-23 23:15:18 +0530

    Add files via upload

---
+++ ifelseternary.py
@@ -0,0 +1,9 @@
+food = input ("food : ")
+eat = "Yes" if food =="cake" else "no"
+print(eat)
+
+food = input("food : ")
+print("sweet") if food == "cake" or food == "jalebi" else print("not sweet")
+
+food = input("food : ")
+print("sweet") if food == "cake" or food == "jalebi" else print("not sweet")
