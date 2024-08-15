99cc16f7de5beb956a3c5287c250f5a09299f2c8
vedantsatote <satoteonline@gmail.com>
2024-08-15 10:45:10 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,10 @@
+class DemoClass:
+    a= 10 
+    def showvalue(self):
+        self.c=self.a*self.a
+        print (self.c)
+    def showvalue1(self):
+            print("WElcome to wscubetech")
+obj=DemoClass()
+obj.showvalue()
+obj.showvalue1()
---
+++ constructor1.py
@@ -0,0 +1,11 @@
+class MyClass:
+    def __init__(self, attribute1, attribute2):
+        self.attribute1 = attribute1
+        self.attribute2 = attribute2
+
+# Creating an instance of MyClass
+obj = MyClass("value1", "value2")
+
+# Accessing the attributes
+print(obj.attribute1)  # Output: value1
+print(obj.attribute2)  # Output: value2
